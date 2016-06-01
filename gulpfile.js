a708a82d298749587afeb953cb8040be82d48c36
Tejas Dinkar <tejas@gja.in>
2016-06-01 18:41:45 +0530

    Correctly asseting the images

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 var elixir = require('laravel-elixir');
 
+var destination = "public/pina-colada/assets";
+var tempPath = "tmp/asset";
+
+elixir.config.publicPath = tempPath;
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('application.scss', tempPath + "/application.css");
 });
 
 elixir(function(mix) {
-    mix.browserify("app.js");
+    mix.browserify("application.js", tempPath + "/application.js");
 });
 
 elixir(function(mix) {
-    mix.version(["js/app.js", "css/app.css", "images"], "public/pina-colada/assets");
+    mix.copy("resources/assets/images", tempPath);
+});
+
+elixir(function(mix) {
+    mix.version([
+      "application.js",
+      "application.css",
+      "**/*.gif",
+      "**/*.jpg",
+      "**/*.png",
+      "**/*.svg"
+    ], destination);
 });

47880677f8f588196781e80b75f7bfc9438e5116
Tejas Dinkar <tejas@gja.in>
2016-06-09 00:47:50 +0530

    Adding client side focussed images

--- resources/assets/js/templates.js
+++ resources/assets/js/templates.js
@@ -1,4 +1,7 @@
 var _ = require("lodash");
+var Twig = require("twig");
+
+var FocussedImage = require("quintype-js").FocussedImage;
 
 global.transformTemplates = function(x) {
   return _.extend(x, {
@@ -11,5 +14,11 @@ var TEMPLATES = {
   "home_body": require("../../../resources/views/home/body.twig"),
   "home_story": require("../../../resources/views/home/story.twig")
 };
+
+Twig.extendFunction("focusedImageUrl", function(slug, aspectRatio, metadata, options) {
+  var cdn = global.qtConfig["image-cdn"];
+  var image = new FocussedImage(slug, metadata);
+  return cdn + "/" + slug + image.path(aspectRatio, options);
+});
 
 module.exports = window.ooga = TEMPLATES;
